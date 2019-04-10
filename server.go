d5dbb3eee99abede56a1b7ba2414cbe2e13a8ac6
Iskandar Pashayev <ispashayev@gmail.com>
2019-04-10 08:59:15 -0700

    ops(*): create new server for app in golang

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  // "fmt"
+  "github.com/gin-gonic/gin"
+  // "net/http"
+)
+
+func main() {
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run() // listen and server on 0.0.0.0:8080
+}
